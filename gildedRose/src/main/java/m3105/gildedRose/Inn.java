ab1a972b5fc853b9f4e71ed7f71d4e24f8e2df1d
iut <iut@10.0.2.15>
2017-11-20 15:31:27 +0100

    Refactoring

--- gildedRose/src/main/java/m3105/gildedRose/Inn.java
+++ gildedRose/src/main/java/m3105/gildedRose/Inn.java
@@ -21,77 +21,83 @@ public class Inn {
 	}
 
 	public void updateQuality() {
-		for (int i = 0; i < items.size(); i++) {
-			// Si l'item n'est pas "Aged Brie" et n'est pas "Backstage ..."
-			if (!items.get(i).getName().equals("Aged Brie")
-					&& !items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-				// Si sa qualité est supérieur à 0
-				if (items.get(i).getQuality() > 0) {
-					// Si ce n'est pas Sulfuras
-					if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
-					}
-				}
-			} else
-			// Sinon si l'item est "Aged Brie" ou "Backstage ..."
-			{
-				// Si la qualité de l'item est inférieur à 50
-				if (items.get(i).getQuality() < 50) {
-					items.get(i).setQuality(items.get(i).getQuality() + 1);
+		for (int numItem = 0; numItem < items.size(); numItem++) {
+			this.updateItemQuality(numItem);
+		}
 
-					// Si l'item est "Backstage ..."
-					if (items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-						// Si le backstage sellin est inférieur à 11
-						if (items.get(i).getSellIn() < 11) {
-							// Si la qualité est ENCORE inférieur à 50
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
+	}
+
+	private void updateItemQuality(int numItem) {
+		// Si l'item n'est pas "Aged Brie" et n'est pas "Backstage ..."
+		if (items.get(numItem).getName().equals("Aged Brie") || items.get(numItem).getName().equals("Backstage passes to a TAFKAL80ETC concert")) // Sinon si l'item est "Aged Brie" ou "Backstage ..."
+		{
+			// Si la qualité de l'item est inférieur à 50
+			if (items.get(numItem).getQuality() < 50) {
+				items.get(numItem).setQuality(items.get(numItem).getQuality() + 1);
+
+				// Si l'item est "Backstage ..."
+				if (items.get(numItem).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					// Si le backstage sellin est inférieur à 11
+					if (items.get(numItem).getSellIn() < 11) {
+						// Si la qualité est ENCORE inférieur à 50
+						if (items.get(numItem).getQuality() < 50) {
+							items.get(numItem).setQuality(items.get(numItem).getQuality() + 1);
 						}
+					}
 
-						// Si le sellin de "Backstage ..." est inf à 6
-						if (items.get(i).getSellIn() < 6) {
-							// Si la qualité est ENCORE inférieur à 50
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
+					// Si le sellin de "Backstage ..." est inf à 6
+					if (items.get(numItem).getSellIn() < 6) {
+						// Si la qualité est ENCORE inférieur à 50
+						if (items.get(numItem).getQuality() < 50) {
+							items.get(numItem).setQuality(items.get(numItem).getQuality() + 1);
 						}
 					}
 				}
 			}
+		} else {
+// Si sa qualité est supérieur à 0
+if (items.get(numItem).getQuality() > 0) {
+		// Si ce n'est pas Sulfuras
+		if (!items.get(numItem).getName().equals("Sulfuras, Hand of Ragnaros")) {
+			items.get(numItem).setQuality(items.get(numItem).getQuality() - 1);
+		}
+}
+}
 
-			// Si l'item n'est pas Sulfuras
-			if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-				// On diminue son sellin de -1
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
+		this.updateItemSellin(numItem);
 
-			// Si le sellin de l'item est inf à 0
-			if (items.get(i).getSellIn() < 0) {
-				// Si l'item n'est pas Aged Brie
-				if (!items.get(i).getName().equals("Aged Brie")) {
-					// Si l'item n'est pas Bakstage ...
-					if (!items.get(i).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-						// Si la qualité de l'item est sup à 0
-						if (items.get(i).getQuality() > 0) {
-							// Si l'item n'est pas Sulfuras
-							if (!items.get(i).getName().equals("Sulfuras, Hand of Ragnaros")) {
-								items.get(i).setQuality(items.get(i).getQuality() - 1);
-							}
+		// Si le sellin de l'item est inf à 0
+		if (items.get(numItem).getSellIn() < 0) {
+			// Si l'item n'est pas Aged Brie
+			if (!items.get(numItem).getName().equals("Aged Brie")) {
+				// Si l'item n'est pas Bakstage ...
+				if (!items.get(numItem).getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+					// Si la qualité de l'item est sup à 0
+					if (items.get(numItem).getQuality() > 0) {
+						// Si l'item n'est pas Sulfuras
+						if (!items.get(numItem).getName().equals("Sulfuras, Hand of Ragnaros")) {
+							items.get(numItem).setQuality(items.get(numItem).getQuality() - 1);
 						}
-					} else {
-						// Si l'item est "Backstage ..."
-						items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
 					}
-					// Si l'item est Aged Brie
 				} else {
-					if (items.get(i).getQuality() < 50) {
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
-					}
+					// Si l'item est "Backstage ..."
+					items.get(numItem).setQuality(0);
+				}
+				// Si l'item est Aged Brie
+			} else {
+				if (items.get(numItem).getQuality() < 50) {
+					items.get(numItem).setQuality(items.get(numItem).getQuality() + 1);
 				}
 			}
 		}
+	}
 
+	private void updateItemSellin(int numItem) {
+		// Si l'item n'est pas Sulfuras
+		if (!items.get(numItem).getName().equals("Sulfuras, Hand of Ragnaros")) {
+			// On diminue son sellin de -1
+			items.get(numItem).setSellIn(items.get(numItem).getSellIn() - 1);
+		}
 	}
 
 	public static void main(String[] args) {
