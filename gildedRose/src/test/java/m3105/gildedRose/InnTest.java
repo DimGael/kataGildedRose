0bd1d3ad3c8c2c31450fc33fcc1bcfefcf4486c6
iut <iut@10.0.2.15>
2017-11-20 14:50:08 +0100

    Création du test de l'état initial

--- gildedRose/src/test/java/m3105/gildedRose/InnTest.java
+++ gildedRose/src/test/java/m3105/gildedRose/InnTest.java
@@ -1,11 +1,18 @@
 package m3105.gildedRose;
 
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InnTest {
 
 	@Test
-	public void qualiteNonNegative() {
-
+	public void should_getItems() {
+		Inn inn = new Inn();
+		assertThat(inn.getItems()).extracting("name").contains("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose",
+													"Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert",
+													"Conjured Mana Cake");
+		assertThat(inn.getItems()).extracting("quality").contains(20, 0, 7 80, 20, 6);
+		
+		assertTaht(inn.getItems()).extracting("sellin")
 	}
 }
